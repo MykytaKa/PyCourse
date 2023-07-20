1890f4ce51f89659c53893e6d9671e5d73e86ca8
Nikita <nikitakalyzhnyi@gmail.com>
2023-07-20 17:04:43 +0300

    improve code

--- Homeworks/Homework_2/task.py
+++ Homeworks/Homework_2/task.py
@@ -1,194 +1,132 @@
 import requests as re
 import csv
 import copy
+from collections import Counter
+import time as t
+from datetime import timedelta, date
 
 class CinemaUser:
-    headers = {
+    HEADERS = {
         "accept": "application/json",
         "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMTI3NGFmYTRlNTUyMjRjYzRlN2Q0NmNlMTNkOTZjOSIsInN1YiI6IjVkNmZhMWZmNzdjMDFmMDAxMDU5NzQ4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lbpgyXlOXwrbY0mUmP-zQpNAMCw_h-oaudAJB6Cn5c8"
     }
 
-    def get_inf_from_desired_pages(self, start, end):
-        for i in range(start, end + 1):
-            url = f'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&sort_by=popularity.desc&page={i}'
-            response = re.get(url, headers=self.headers)
-            print(response.json())
+    def __init__(self, number):
+        self.data = []
+        self.collection_of_structures = []
+        self.fetch_data_from_desired_pages(number)
 
-    def get_all_data_from_page(self, page_numb):
-        url = f'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&sort_by=popularity.desc&page={page_numb}'
-        response = re.get(url, headers=self.headers)
-        print(f'Get: {response}\nJson: {response.json()}\nHeaders: {response.headers}\nText: {response.text}\nContent: {response.content}')
+    def get_response(self, number):
+        url = f'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&sort_by=popularity.desc&page={number}'
+        return re.get(url, headers=self.HEADERS)
 
-    def get_data_about_movies(self, start, end, step, page):
-        url = f'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&sort_by=popularity.desc&page={page}'
-        response = re.get(url, headers=self.headers)
-        information = response.json()
-        for i in range(start, end + 1, step):
-            films = information['results']
-            print(films[i]['original_title'])
+    def fetch_data_from_desired_pages(self, end):
+        for i in range(1, end + 1):
+            response = self.get_response(i)
+            self.data.extend(response.json()['results'])
+
+    def get_all_data_from_page(self):
+        return self.data
+
+    def get_data_about_movies(self, start, end, step):
+        return self.data[start:end:step]
 
     def get_the_most_popular_movie(self):
-        url = f'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&sort_by=popularity.desc&page={1}'
-        response = re.get(url, headers=self.headers)
-        information = response.json()
-        the_most_popular_movie_popularity = information['results'][0]['popularity']
-        the_most_popular_movie_title = information['results'][0]['original_title']
-        for movie in information['results']:
-            if movie['popularity'] > the_most_popular_movie_popularity:
-                the_most_popular_movie_popularity = movie['popularity']
-                the_most_popular_movie_title = movie['original_title']
-        print(the_most_popular_movie_title)
+        return max(self.data, key=lambda x: x['popularity'])['original_title']
 
     def find_movie(self, find_text):
-        url = f'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&sort_by=popularity.desc&page={1}'
-        response = re.get(url, headers=self.headers)
-        information = response.json()
-        for film in information['results']:
-            if film['overview'].find(find_text) > 0:
-                print(film['original_title'])
+        necessary_movies = [film['original_title'] for film in self.data if film['overview'].find(find_text) > 0]
+        return necessary_movies
 
     def print_genres(self):
-        genre_url = 'https://api.themoviedb.org/3/genre/movie/list?language=en'
-        genre_response = re.get(genre_url, headers=self.headers)
-        genre_information = genre_response.json()
-        print(genre_information['genres'])
+        genre_list = set(genre for film in self.data for genre in film['genre_ids'])
+        return genre_list
 
     def delete_movies_with_genre(self, deleted_genre):
-        url = f'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&sort_by=popularity.desc&page={1}'
-        response = re.get(url, headers=self.headers)
-        information = response.json()
-        movies = information['results']
-        k = 0
-        for i in movies:
-            for genre in movies[k]['genre_ids']:
-                if genre == deleted_genre:
-                    movies.pop(k)
-            k += 1
-        for film in information['results']:
-            print(film['original_title'])
+        return [film['original_title'] for film in self.data if deleted_genre not in film['genre_ids']]
 
-# ERRRRRRRRRRRRRRRROOOOOOOOOORRRRRR
-    # def get_most_popular_genre(self):
-    #     url = f'https://https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&sort_by=popularity.desc&page={1}'
-    #     response = re.get(url, headers=self.headers)
-    #     information = response.json()
-    #
-    #     genre_url = 'https://api.themoviedb.org/3/genre/movie/list?language=en'
-    #     genre_response = re.get(genre_url, headers=self.headers)
-    #     genre_information = genre_response.json()
-    #
-    #     genre_popularity = []
-    #     print(genre_popularity)
+    def get_most_popular_genre(self):
+        genres_popularity = [genre for film in self.data for genre in film['genre_ids']]
+        return Counter(genres_popularity).most_common(1)[0][0]
 
-
-    def group_movies(self, common_genre):
-        url = f'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&sort_by=popularity.desc&page={1}'
-        response = re.get(url, headers=self.headers)
-        information = response.json()
-        movies = information['results']
-        k = 0
-        common_movies = []
-        for i in movies:
-            for genre in movies[k]['genre_ids']:
-                if genre == common_genre:
-                    common_movies.append(movies[k]['original_title'])
-            k += 1
-        print(common_movies)
+    def group_movies(self):
+        common_movies = [{film['original_title'], movie['original_title']} for i, film in enumerate(self.data) for genre in film['genre_ids'] for movie in self.data[i+1::] if genre in movie['genre_ids']]
+        # I left it here because the top line is a little bit too long, but it is written in one line :)
+        #
+        # for i, film in enumerate(self.data):
+        #     for genre in film['genre_ids']:
+        #         for movie in self.data[i+1::]:
+        #             if genre in movie['genre_ids']:
+        #                 common_movies.append({film['original_title'], movie['original_title']})
+        return common_movies
 
     def replace_genre(self):
-        url = f'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&sort_by=popularity.desc&page={1}'
-        response = re.get(url, headers=self.headers)
-        information = response.json()
-        movies = information['results']
-        movies_copy = copy.deepcopy(movies)
-        print(f'Default data:{movies}')
-        for i in range(len(movies_copy)):
-            movies_copy[i]['genre_ids'][0] = 22
+        movies_copy = copy.deepcopy(self.data)
+        print(f'Default data:{self.data}')
+        movies_copy = list(map(change_genre_id, movies_copy))
         print(f'Changed data:{movies_copy}')
 
     def print_collection_of_structures(self):
-        url = f'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&sort_by=popularity.desc&page={1}'
-        response = re.get(url, headers=self.headers)
-        information = response.json()
-        movies = information['results']
-        collection_of_structures = []
-        for i in range(len(movies)):
+        for film in self.data:
             example = {'Title': '',
                        'Popularity': 0,
                        'Score': 0,
                        'Last_day_in_cinema': ''}
-            example['Title'] = movies[i]['original_title']
-            example['Popularity'] = round(movies[i]['popularity'], 1)
-            example['Score'] = int(movies[i]['vote_average'])
-            tmp = movies[i]['release_date']
-#           Converting date
-            tmp_list = tmp.split('-')
-            for m in range(len(tmp_list)):
-                tmp_list[m] = int(tmp_list[m])
-            tmp_list[1] += 2
-            tmp_list[2] += 14
-            while True:
-                while True:
-                    if tmp_list[2] > 31:
-                        tmp_list[1] += 1
-                        tmp_list[2] -= 31
-                    else:
-                        break
-                if tmp_list[1] > 12:
-                    tmp_list[0] += 1
-                    tmp_list[1] -= 12
-                else:
-                    break
-            for m in range(len(tmp_list)):
-                tmp_list[m] = str(tmp_list[m])
-            tmp = "-".join(tmp_list)
-#           Converting data
-            example['Last_day_in_cinema'] = tmp
-            collection_of_structures.append(example)
-        for element in collection_of_structures:
-            print(element)
+            example['Title'] = film['original_title']
+            example['Popularity'] = round(film['popularity'], 1)
+            example['Score'] = int(film['vote_average'])
+            date_str = film['release_date'].split('-')
+            date_int = [int(element) for element in date_str]
+            release_date = date(date_int[0], date_int[1], date_int[2]) + timedelta(weeks=6, days=2)
+            example['Last_day_in_cinema'] = release_date.strftime('%Y-%m-%d')
+            self.collection_of_structures.append(example)
+        return self.collection_of_structures
 
-        return collection_of_structures
+    def write_collection_to_file(self, path):
+        with open(path, 'w', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(['Data'])
+            writer.writerows(data)
 
 
-user = CinemaUser()
+def change_genre_id(movie):
+    movie['genre_ids'][0] = 22
+    return movie
 
 print('Task 1:')
-user.get_inf_from_desired_pages(1, 3)
+user = CinemaUser(1)
 
 print('\nTask 2:')
-user.get_all_data_from_page(3)
+user.get_all_data_from_page()
 
 print('\nTask 3:')
-user.get_data_about_movies(3, 19, 4, 1)
+print(user.get_data_about_movies(3, 19, 4))
 
 print('\nTask 4:')
-user.get_the_most_popular_movie()
+print(user.get_the_most_popular_movie())
 
 print('\nTask 5:')
-user.find_movie('human')
+print(user.find_movie('human'))
 
 print('\nTask 6:')
-user.print_genres()
+print(user.print_genres())
 
 print('\nTask 7:')
-user.delete_movies_with_genre(28)
+print(user.delete_movies_with_genre(28))
 
 print('\nTask 8:')
-# user.get_most_popular_genre()
+print(user.get_most_popular_genre())
 
 print('\nTask 9:')
-user.group_movies(16)
+print(user.group_movies())
 
 print('\nTask 10:')
 user.replace_genre()
 
 print('\nTask 11:')
 data = user.print_collection_of_structures()
+for movie in data:
+    print(movie)
 
 print('\nTask 12:')
-# file_path = 'D:/'
-# with open(file_path, 'w', newline='') as csv_file:
-#     writer = csv.writer(csv_file)
-#     writer.writerow(['Data'])
-#     writer.writerows(data)
+user.write_collection_to_file('C:/file.csv')
