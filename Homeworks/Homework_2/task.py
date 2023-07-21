0510b064025d5f62b01b0c1e40803cd177c1f841
Nikita <nikitakalyzhnyi@gmail.com>
2023-07-21 23:59:36 +0300

    used your tips to improve the code

--- Homeworks/Homework_2/task.py
+++ Homeworks/Homework_2/task.py
@@ -3,22 +3,22 @@ import csv
 import copy
 from collections import Counter
 import time as t
-from datetime import timedelta, date
+from datetime import timedelta, date, datetime
 
 
 class CinemaUser:
     HEADERS = {
-        "accept": "application/json",
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMTI3NGFmYTRlNTUyMjRjYzRlN2Q0NmNlMTNkOTZjOSIsInN1YiI6IjVkNmZhMWZmNzdjMDFmMDAxMDU5NzQ4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lbpgyXlOXwrbY0mUmP-zQpNAMCw_h-oaudAJB6Cn5c8"
+        'accept': 'application/json',
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMTI3NGFmYTRlNTUyMjRjYzRlN2Q0NmNlMTNkOTZjOSIsInN1YiI6IjVkNmZhMWZmNzdjMDFmMDAxMDU5NzQ4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lbpgyXlOXwrbY0mUmP-zQpNAMCw_h-oaudAJB6Cn5c8'
     }
 
-    def __init__(self, number):
+    def __init__(self, number_of_pages):
         self.data = []
         self.collection_of_structures = []
-        self.fetch_data_from_desired_pages(number)
+        self.fetch_data_from_desired_pages(number_of_pages)
 
-    def get_response(self, number):
-        url = f'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&sort_by=popularity.desc&page={number}'
+    def get_response(self, number_of_page):
+        url = f'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&sort_by=popularity.desc&page={number_of_page}'
         return re.get(url, headers=self.HEADERS)
 
     def fetch_data_from_desired_pages(self, end):
@@ -36,10 +36,10 @@ class CinemaUser:
         return max(self.data, key=lambda x: x['popularity'])['original_title']
 
     def find_movie(self, find_text):
-        return [film['original_title'] for film in self.data if film['overview'].find(find_text) > 0]
+        return [film['original_title'] for film in self.data if find_text in film['overview']]
 
-    def print_genres(self):
-        return set(genre for film in self.data for genre in film['genre_ids'])
+    def get_present_genres(self):
+        return tuple({genre for film in self.data for genre in film['genre_ids']})
 
     def delete_movies_with_genre(self, deleted_genre):
         return [film['original_title'] for film in self.data if deleted_genre not in film['genre_ids']]
@@ -48,48 +48,36 @@ class CinemaUser:
         return Counter([genre for film in self.data for genre in film['genre_ids']]).most_common(1)[0][0]
 
     def group_movies(self):
-        common_movies = [{film['original_title'], movie['original_title']} for i, film in enumerate(self.data) for genre in film['genre_ids'] for movie in self.data[i+1::] if genre in movie['genre_ids']]
-        # I left it here because the top line is a little bit too long, but it is written in one line :)
-        #
-        # for i, film in enumerate(self.data):
-        #     for genre in film['genre_ids']:
-        #         for movie in self.data[i+1::]:
-        #             if genre in movie['genre_ids']:
-        #                 common_movies.append({film['original_title'], movie['original_title']})
+        common_movies = [(film['original_title'], movie['original_title'])
+                         for i, film in enumerate(self.data)
+                         for genre in film['genre_ids']
+                         for movie in self.data
+                         if genre in movie['genre_ids']]
         return common_movies
 
-    def replace_genre(self):
-        movies_copy = copy.deepcopy(self.data)
-        print(f'Default data:{self.data}')
-        movies_copy = list(map(change_genre_id, movies_copy))
-        print(f'Changed data:{movies_copy}')
+    def change_genre_id(self, movie):
+        movie['genre_ids'][0] = 22
+        return movie
+
+    def get_original_and_copy_data(self):
+        return self.data, list(map(self.change_genre_id, copy.deepcopy(self.data)))
 
     def print_collection_of_structures(self):
         for film in self.data:
-            example = {'Title': '',
-                       'Popularity': 0,
-                       'Score': 0,
-                       'Last_day_in_cinema': ''}
-            example['Title'] = film['original_title']
-            example['Popularity'] = round(film['popularity'], 1)
-            example['Score'] = int(film['vote_average'])
-            date_str = film['release_date'].split('-')
-            date_int = [int(element) for element in date_str]
-            release_date = date(date_int[0], date_int[1], date_int[2]) + timedelta(weeks=10, days=4)
-            example['Last_day_in_cinema'] = release_date.strftime('%Y-%m-%d')
+            example = {'Title': film['original_title'],
+                       'Popularity': round(film['popularity'], 1),
+                       'Score': int(film['vote_average']),
+                       'Last_day_in_cinema': (datetime.strptime(film['release_date'], "%Y-%m-%d") + timedelta(weeks=10, days=4)).strftime('%Y-%m-%d')}
             self.collection_of_structures.append(example)
         return self.collection_of_structures
 
     def write_collection_to_file(self, path):
         with open(path, 'w', newline='') as csv_file:
-            writer = csv.writer(csv_file)
-            writer.writerow(['Data'])
-            writer.writerows(data)
-
-
-def change_genre_id(movie):
-    movie['genre_ids'][0] = 22
-    return movie
+            fieldnames = ['Title', 'Popularity', 'Score', 'Last_day_in_cinema']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+            for film in self.collection_of_structures:
+                writer.writerow(film)
 
 
 print('Task 1:')
@@ -108,7 +96,7 @@ print('\nTask 5:')
 print(user.find_movie('human'))
 
 print('\nTask 6:')
-print(user.print_genres())
+print(user.get_present_genres())
 
 print('\nTask 7:')
 print(user.delete_movies_with_genre(28))
@@ -120,7 +108,7 @@ print('\nTask 9:')
 print(user.group_movies())
 
 print('\nTask 10:')
-user.replace_genre()
+print(f'{user.get_original_and_copy_data()[0]}\n{user.get_original_and_copy_data()[1]}')
 
 print('\nTask 11:')
 data = user.print_collection_of_structures()
@@ -128,4 +116,4 @@ for movie in data:
     print(movie)
 
 print('\nTask 12:')
-user.write_collection_to_file('C:/file.csv')
+user.write_collection_to_file('file.csv')
